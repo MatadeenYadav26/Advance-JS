bec113b12ec565e714e24b4ab99395e983b9c34c
Matadeen Yadav <matadeenyadav202@gmail.com>
2026-05-12 00:55:48 +0530

    Add files via upload

---
+++ 2. Day - 59 : Advance Js :  Async Await Callback/day59.js
@@ -0,0 +1,31 @@
+// callback
+
+// function abcd(fn){
+//     fn(function(fn2){
+//         fn2(function(){
+//             console.log("hiiii");
+            
+//         });
+//     });
+// }
+
+// abcd(function (fn){
+//     fn(function(fn3){
+//         fn3();  
+//     });
+//     console.log("hey");
+// })
+
+// function amanSeDetailLao(address,cb){
+//     console.log("fetching details...");
+    
+//     setTimeout(()=>{
+//         cb({lat:23.36, lng: 76.5});
+//     },3000);
+// }
+
+// amanSeDetailLao("althan",function(details){
+//     console.log(details);
+// });
+
+
